a8da9d959c49ba068aeb5c668a6131b715aa893b
Harley Rayner <harleymteden@gmail.com>
2018-09-17 18:22:14 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,5 +5,6 @@ def create_project_hash
   # This just opens a file and reads it into a variable
   html = File.read("fixtures/kickstarter.html")
   kickstarter = Nokogiri::HTML(html)
-  
 end
+
+binding.pry
